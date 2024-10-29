acd0d9307c971f484e6ef1e62322adc048f87fc1
grand00father <121361298+grand00father@users.noreply.github.com>
2024-10-29 23:19:39 +0300

    Update WLC.h

--- WLC.h
+++ WLC.h
@@ -1,8 +1,9 @@
-#pragma once
 #ifndef WLC_H
 #define WLC_H
 
+#include<iostream>
 #include<vector>
+#include<limits>
 
 int WLC(const std::vector<int>& count_queue, const std::vector<float>& load_CPU, const std::vector<float>& RAM, const std::vector<float>& power_CPU, float max_RAM, float max_power_CPU);
 
