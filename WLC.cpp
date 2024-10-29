fa1f4da01e75affba2bff8714196d00c19854374
grand00father <121361298+grand00father@users.noreply.github.com>
2024-10-29 23:20:34 +0300

    Update WLC.cpp

--- WLC.cpp
+++ WLC.cpp
@@ -1,20 +1,17 @@
-#include<iostream>
-#include<vector>
-#include<limits>
-using namespace std;
+#include "WLC.h"
 
-//веса характеристик в зависимости от типов задач
+//weights for characteristics depending on the types of tasks
 float weight_load = 0.3;
 float weight_RAM = 0.2;
 float weight_power = 0.5;
 
-int WLC(const vector<int>& count_queue, const vector<float>& load_CPU, const vector<float>& RAM, const vector<float>& power_CPU, float max_RAM, float max_power_CPU) {
+int WLC(const std::vector<int>& count_queue, const std::vector<float>& load_CPU, const std::vector<float>& RAM, const std::vector<float>& power_CPU, float max_RAM, float max_power_CPU) {
 	float weight;
 	float priority;
-	float min_priority = numeric_limits<float>::infinity();
+	float min_priority = std::numeric_limits<float>::infinity();
 	int node = 0;
 	for (int i = 0; i < load_CPU.size(); i++) {
-		weight = weight_load * (load_CPU[i] / 100) + weight_RAM * (RAM[i] / max_RAM)	//вес узла = взвешенная сумма характеристик
+		weight = weight_load * (load_CPU[i] / 100) + weight_RAM * (RAM[i] / max_RAM)	//weight of the node = weighted sum of characteristics
 			+ weight_power *(power_CPU[i] / max_power_CPU);
 
 		priority = count_queue[i] / weight;
@@ -24,5 +21,5 @@ int WLC(const vector<int>& count_queue, const vector<float>& load_CPU, const vec
 			node = i+1;
 		}
 	}
-	return node;	//функция вовращает номер выбранного узла (начинается с 1)
+	return node;	//function returns a number of the choosing node (starting from 1)
 }
