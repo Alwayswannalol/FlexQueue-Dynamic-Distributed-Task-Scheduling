d7445ea9e1f5ecb5f2666b4cf996a90d74aa0b99
Гусев Егор Витальевич <st086931@student.spbu.ru>
2024-12-11 11:20:07 +0300

    Добавил тестовые фотографии и rpc для определения сервера для выполнения задачи

--- src/client.cpp
+++ src/client.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc == 2) {
-        async_client client(grpc::CreateChannel(argv[1], grpc::InsecureChannelCredentials()), "alwayswannalol");
+        async_client client_distribution(grpc::CreateChannel(argv[1], grpc::InsecureChannelCredentials()), "alwayswannalol");
 
         std::filesystem::create_directories("tmp/results");
 
@@ -12,8 +12,8 @@ int main(int argc, char* argv[]) {
         std::condition_variable cv;
         std::mutex mtx;
 
-        std::thread response_thread([&]() {
-            client.handle_call(requests_count, cv, response);
+        std::thread distribution_thread([&]() {
+            client_distribution.handle_call(requests_count, cv, response);
         });
 
         std::vector<std::string> filePaths = {"client_photos/client/1.jpg",
@@ -22,15 +22,22 @@ int main(int argc, char* argv[]) {
                                               
         task_info task(filePaths.size());
 
-        client.async_distribute_detection_task(task);
+        client_distribution.async_distribute_detection_task(task);
 
         std::unique_lock<std::mutex> lock(mtx);
         cv.wait(lock, [&requests_count]() { return requests_count.load() == 0; });
 
-        client.async_execute_detection_task(filePaths);
+        async_client client_execution(grpc::CreateChannel(argv[1], grpc::InsecureChannelCredentials()), "alwayswannalol");
+
+        std::thread execution_thread([&]() {
+            client_execution.handle_call(requests_count, cv, response);
+        });
+
+        client_execution.async_execute_detection_task(filePaths);
 
         // Ждем завершения потока обработки ответов
-        response_thread.join();
+        distribution_thread.join();
+        execution_thread.join();
     }
     else {
         std::cerr << "Error" << std::endl;
